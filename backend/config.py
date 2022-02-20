53d0b6eeeb39715a827817146fcb9f44d380ce56
Vikrame Vasudev <vikrame1999@gmail.com>
2022-02-20 05:05:44 -0800

    removed api key

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
 DIALOGFLOW_PROJECT_ID = 'hospitalhoppers-vtcl'
 DIALOGFLOW_LANGUAGE_CODE = 'en-US'
 SESSION_ID = 'random_string_hash'
-API_KEY = 'AIzaSyDI67KIvJoLoMruYCB052Vt2pUgQm6bd48'
+API_KEY = '<API_KEY>'
 
 TEXT_SEARCH_API = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
 INTERSYSTEM_API_URL = 'https://fhir.2uqaf9xphkny.static-test-account.isccloud.io'
-INTERSYSTEM_API_KEY = 'Vzwn0NykbG9IqjCXYJAlg1ySvetwAGhU3ZFevAJN'
+INTERSYSTEM_API_KEY = '<API_KEY>'
 PATIENT_ENDPOINT = "/Patient"
 QUESTIONNAIRE_RESPONSE_ENDPOINT = '/QuestionnaireResponse'
 GOAL_ENDPOINT = '/Goal'
