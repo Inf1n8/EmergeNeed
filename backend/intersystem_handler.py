f0aff9aa5edb95ceea37518baea04e4259b20f75
Vikrame Vasudev <vikrame1999@gmail.com>
2022-02-20 00:51:43 -0800

    Intersystem APIs integrated

--- backend/intersystem_handler.py
+++ backend/intersystem_handler.py
@@ -1,6 +1,16 @@
 import uuid
 import requests
-from config import (PATIENT_ENDPOINT, INTERSYSTEM_API_URL, INTERSYSTEM_API_KEY)
+from datetime import datetime
+from config import (
+    PATIENT_ENDPOINT, 
+    INTERSYSTEM_API_URL, 
+    INTERSYSTEM_API_KEY, 
+    QUESTIONNAIRE_ENDPOINT, 
+    GOAL_ENDPOINT, 
+    QUESTIONNAIRE_RESPONSE_ENDPOINT,
+    IMMUNIZATION_ENDPOINT,
+    APPOINTMENT_ENDPOINT
+)
 
 def get_random_id():
     return uuid.uuid4()
@@ -28,3 +38,84 @@ def create_patient_record(data):
     res = intersystem_post_request_handler(PATIENT_ENDPOINT, data)
     return res
     
+def create_questionnaire_response(data):
+    data = {**data, 'language': 'english-US', 'authored': str(datetime.now()), 'resourceType': 'QuestionnaireResponse', 
+    'id': get_random_id(),
+    "author": {
+        "reference": "http://hl7.org/fhir/Practitioner/example",
+        "type": "Practitioner"
+    },  "subject": {
+    "reference": "http://hl7.org/fhir/Patient/1",
+    "type": "Patient"
+    }}
+    res = intersystem_post_request_handler(QUESTIONNAIRE_RESPONSE_ENDPOINT, data)
+    return res
+
+def create_goal(data):
+    data = {**data, 'language': 'english-US', 'resourceType': 'Goal', 'id': get_random_id(),  'note': [{
+        'id': get_random_id(),
+        'text': 'Feeling relaxed',
+        'time': str(datetime.now()),
+        'priority': {
+            'id': get_random_id(),
+            'description': 'high-priority'
+        }
+    }]}
+    res = intersystem_post_request_handler(GOAL_ENDPOINT, data)
+    return res
+
+def create_questionnaire(data):
+    data = {**data, 'language': 'english-US', 'date': str(datetime.now()), 'resourceType': 'Questionnaire', 
+    'id': get_random_id(),
+    'description': 'Wellness assessment',
+    'item': [{
+        'id': get_random_id(),
+        'type': 'integer',
+        'text': 'On a scale of 1-10, how do you feel today?'
+    }],
+    "publisher": "Dr. Henry",  "subject": {
+    "reference": "http://hl7.org/fhir/Patient/1",
+    "type": "Patient"
+    }}
+    res = intersystem_post_request_handler(QUESTIONNAIRE_ENDPOINT, data)
+    return res
+
+
+def create_immunization(data):
+    data = {**data, 'id': get_random_id(), 'language': 'english-US',
+    "resourceType": "Immunization", 
+    'manufacturer': {
+        'id': get_random_id(),
+        'display': 'Pfizer'
+    },
+    "patient": {
+    "reference": "http://hl7.org/fhir/Patient/1",
+    "type": "Patient"
+    }}
+    res = intersystem_post_request_handler(IMMUNIZATION_ENDPOINT, data)
+    return res
+
+
+def create_appointment(data):
+    data = {**data, 'id': get_random_id(), 'language': 'english-US',
+    "resourceType": "Appointment",
+    "priority": 5,
+    "appointmentType": {
+    "coding": [
+      {
+        "system": "http://terminology.hl7.org/CodeSystem/v2-0276",
+        "code": "WALKIN",
+        "display": "A previously unscheduled walk-in visit"
+      }
+    ]
+  },
+    'manufacturer': {
+        'id': get_random_id(),
+        'display': 'Pfizer'
+    },
+    "patient": {
+    "reference": "http://hl7.org/fhir/Patient/1",
+    "type": "Patient"
+    }}
+    res = intersystem_post_request_handler(APPOINTMENT_ENDPOINT, data)
+    return res
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,14 @@ import google.cloud.dialogflow_v2 as dialogflow
 from ner import get_entities
 from config import (DIALOGFLOW_PROJECT_ID, SESSION_ID)
 from rec_hospitals import get_hospital_recommendations
-from intersystem_handler import create_patient_record
+from intersystem_handler import (
+    create_patient_record, 
+    create_questionnaire_response, 
+    create_goal, 
+    create_questionnaire,
+    create_immunization,
+    create_appointment
+)
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './hospitalhoppers-6c9e1bacd23a.json'
 
@@ -36,10 +43,47 @@ class Patient(Resource):
         res = create_patient_record(payload)
         return res.json()
 
+class QuestionnaireResponse(Resource):
+    def post(self):
+        payload = request.get_json()
+        res = create_questionnaire_response(payload)
+        return res.json()
+
+
+class Questionnaire(Resource):
+    def post(self):
+        payload = request.get_json()
+        res = create_questionnaire(payload)
+        return res.json()
+
+class Goal(Resource):
+    def post(self):
+        payload = request.get_json()
+        res = create_goal(payload)
+        return res.json()
+
+
+class Immunization(Resource):
+    def post(self):
+        payload = request.get_json()
+        res = create_immunization(payload)
+        return res.json()
+
+class Appointment(Resource):
+    def post(self):
+        payload = request.get_json()
+        res = create_appointment(payload)
+        return res.json()
+
 
 api.add_resource(IntentRecognition, '/intent')
 api.add_resource(HospitalList, '/hospitals')
 api.add_resource(Patient, '/patient')
+api.add_resource(QuestionnaireResponse, '/questionnaireResponse')
+api.add_resource(Questionnaire, '/questionnaire')
+api.add_resource(Goal, '/goal')
+api.add_resource(Immunization, '/immunization')
+api.add_resource(Appointment, '/appointment')
 
 if __name__ == '__main__':
     app.run(debug=True)
